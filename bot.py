ef9d06d1417388dad1765e77f31b977b92af41cf
jaffer-dev133 <affanoioffical@gmail.com>
2026-01-13 11:25:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,15 +4,34 @@
 
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
+from flask import Flask
+from threading import Thread
+import os
+
+# --- FLASK SERVER START ---
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is Running Safely!"
+
+def run_flask():
+    # Render takes the port from environment variables
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run_flask)
+    t.start()
+# --- FLASK SERVER END ---
 
 if STRING_SESSION is not None and LOGIN_SYSTEM == False:
-	TechVJUser = Client("TechVJ", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
-	TechVJUser.start()
+    TechVJUser = Client("TechVJ", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
+    TechVJUser.start()
 else:
     TechVJUser = None
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
             "techvj login",
@@ -24,21 +43,20 @@ class Bot(Client):
             sleep_threshold=5
         )
 
-      
     async def start(self):
-            
         await super().start()
         print('Bot Started Powered By @VJ_Bots')
 
     async def stop(self, *args):
-
         await super().stop()
         print('Bot Stopped Bye')
 
 if __name__ == "__main__":
+    # Flask server ko background mein start karein
+    keep_alive() 
+    
+    # Bot ko start karein
     bot = Bot()
     bot.run()
 
 # Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
